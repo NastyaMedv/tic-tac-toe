5e8352fec7d307e3bbe5a490daef75d838784291
Anastasiya Medvedeva <>
2019-10-10 14:50:28 +0300

    part of solution
    getCurrentPlayerSymbol, getFieldValue  and noMoreTurns

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,29 @@
 class TicTacToe {
     constructor() {
-
+      this.gameField = [];
+      for (let i=0;i<3;i++) {
+        this.gameField[i] = ['-','-','-'];
+      }
+      this.current = 'x';
+      //console.log(this.gameField);
+      //console.log(this.current);
     }
 
     getCurrentPlayerSymbol() {
+        return this.current;
+    }
 
+    changeCur() {
+      if (this.current == 'x')
+        { this.current = 'o';}
+        else  { this.current = 'x';}
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+      if (this.gameField[rowIndex][columnIndex]=='-') {
+        this.gameField[rowIndex][columnIndex] = this.current;
+        this.changeCur();
+      }
     }
 
     isFinished() {
@@ -20,7 +35,14 @@ class TicTacToe {
     }
 
     noMoreTurns() {
-
+      let count = 0;
+      for (let i=0;i<3;i++) {
+        for (let j=0;j<3;j++) {
+          if (this.gameField[i][j]!='-')
+           count++;
+        }
+      } 
+      return (count==9) ?  true : false;
     }
 
     isDraw() {
@@ -28,7 +50,11 @@ class TicTacToe {
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+      if (this.gameField[rowIndex][colIndex]!='-') {
+        return this.gameField[rowIndex][colIndex];
+      } else {
+        return null;
+      }
     }
 }
 
